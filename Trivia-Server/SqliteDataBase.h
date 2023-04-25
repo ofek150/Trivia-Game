84f32831b9a3386881e67bfee2bdb42fd71bfa23
danielmordo <102387603+danielmordo@users.noreply.github.com>
2023-04-25 17:14:19 +0300

    Created IDatabase, SqliteDatabase classes and added dependencies
    Declared all of the needed functions in IDatabase,SqliteDatabase classes.

---
+++ Trivia-Server/SqliteDataBase.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "IDatabase.h"
+#include <string>
+#include <iostream>
+#include "sqlite3.h"
+#include <io.h>
+class SqliteDataBase : public IDatabse
+{
+	bool open()override;
+	void close() override;
+	void clear() override;
+	void insertUserIntoDB(const std::string& username, const std::string& password, const std::string& email) override;
+	// queries
+	bool doesUserExist(const std::string& username) override;
+	bool isPasswordValid(const std::string& username, const std::string& password) override;
+
+};
+
